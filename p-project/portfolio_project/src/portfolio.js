776ee1357ddc5aa5a24d660bb3213c78c294dbc2
selva-arsl <selvasamprash777@gmail.com>
2024-03-19 20:33:21 +0530

    portfolio small update

--- p-project/portfolio_project/src/portfolio.js
+++ p-project/portfolio_project/src/portfolio.js
@@ -1,14 +1,116 @@
 import React from "react";
 import "./css/portfolio.css";
+import First_img from "./assets/first.jpg";
 
 class Portfolio extends React.Component {
     render() {
         return (
-            <div className="portfolio-container">
+            <>
+                <div className="mywork-container">
 
-                <h1>portfolio</h1>
+                    <h1>MY <span>WORK</span></h1>
 
-            </div>
+                </div>
+
+
+                <div>
+
+                    <div className="overall">
+
+                        <div className="container">
+
+                            <img src={First_img} alt="img" className="image" />
+
+                            <div className="overlay overlayLeft">
+
+                                <div className="text">Left</div>
+
+                            </div>
+
+
+                        </div>
+
+
+                        <div className="container">
+
+                            <img src={First_img} alt="img" className="image" />
+
+                            <div className="overlay overlayRight">
+
+                                <div className="text">Right</div>
+
+                            </div>
+
+
+                        </div>
+
+
+                        <div className="container">
+
+                            <img src={First_img} alt="img" className="image" />
+
+                            <div className="overlay overlayFade">
+
+                                <div className="text">Fade</div>
+
+                            </div>
+
+
+                        </div>
+
+
+
+                        <div className="container">
+
+                            <img src={First_img} alt="img" className="image" />
+
+                            <div className="overlay overlayTop">
+
+                                <div className="text">Top</div>
+
+                            </div>
+
+
+                        </div>
+
+
+                        <div className="container">
+
+                            <img src={First_img} alt="img" className="image" />
+
+                            <div className="overlay overlayBottom">
+
+                                <div className="text">Bottom</div>
+
+                            </div>
+
+
+                        </div>
+
+
+                        <div className="container">
+
+                            <img src={First_img} alt="img" className="image" />
+
+                            <div className="overlay overlayCross">
+
+                                <div className="text">Cross</div>
+
+                            </div>
+
+
+                        </div>
+
+
+
+                    </div>
+
+
+                </div>
+
+            </>
+
+
         );
     };
 };
