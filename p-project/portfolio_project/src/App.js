fba7b9462c79dab2bc660d7eab69a5641762bb0b
selva-arsl <selvasamprash777@gmail.com>
2024-03-21 12:21:05 +0530

    update works

--- p-project/portfolio_project/src/App.js
+++ p-project/portfolio_project/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Nav_bars from "./nav.js";
+import Nav from "./nav.js";
 import Home from "./home.js";
 import About from "./about.js";
 import Portfolio from './portfolio.js';
@@ -10,13 +10,14 @@ import Contact from './contact.js';
 import Footer from "./footer.js";
 
 
+
 function App() {
   return (
     <div className="App">
 
 
       <BrowserRouter>
-        <Nav_bars />
+        <Nav/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
